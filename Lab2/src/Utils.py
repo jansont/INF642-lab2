031132411002c454aa1a270da2451da02f7a71ab
Theo Janson <tj@sfjtechnologies.com>
2023-01-19 13:52:32 +0100

    all

---
+++ Lab2/src/Utils.py
@@ -0,0 +1,33 @@
+def encode(E1, E2, E3, E4, E5, E6, E7):
+    return E1, E2, E3, E4, E5, E6, E7
+
+
+def tf_encode(x, z1, z2,z4, z5, z6, z7):
+    result_x, result_z1, result_z2, result_z4, result_z5, result_z6, result_z7 = tf.py_function(encode, [x, z1, z2,z4, z5, z6, z7], [tf.int64, tf.int64, tf.int64, tf.int64, tf.int64,tf.int64,tf.int64])
+
+    result_x.set_shape([None])
+    result_z1.set_shape([None])
+    result_z2.set_shape([None])
+    result_z4.set_shape([None])
+    result_z5.set_shape([None])
+    result_z6.set_shape([None])
+    result_z7.set_shape([None])
+
+    return result_x, result_z1, result_z2, result_z4, result_z5, result_z6, result_z7
+
+BUFFER_SIZE = 20000
+BATCH_SIZE = 64
+
+# Creating dataset
+dataset_train = tf.data.Dataset.from_tensor_slices((F0train, AU1_train, AU2_train, AU4_train, AU5_train, AU6_train, AU7_train))
+dataset_validate = tf.data.Dataset.from_tensor_slices((F0validate, AU1_validate, AU2_validate, AU4_validate, AU5_validate, AU6_validate, AU7_validate))
+dataset_test = tf.data.Dataset.from_tensor_slices((F0test, AU1_test, AU2_test, AU4_test, AU5_test, AU6_test, AU7_test))
+dataset_train = dataset_train.map(tf_encode)
+dataset_train = dataset_train.cache()
+dataset_train = dataset_train.shuffle(BUFFER_SIZE).padded_batch(BATCH_SIZE)
+dataset_train = dataset_train.prefetch(tf.data.experimental.AUTOTUNE)
+dataset_validate = dataset_validate.map(tf_encode)
+dataset_validate = dataset_validate.shuffle(BUFFER_SIZE).padded_batch(BATCH_SIZE)
+dataset_test = dataset_test.map(tf_encode)
+dataset_test = dataset_test.cache()
+dataset_test = dataset_test.shuffle(BUFFER_SIZE).padded_batch(BATCH_SIZE_TEST)
---
+++ Lab2/src/encoder.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from utils import *
+from EncoderLayer import *
+from tensorflow.keras import layers
+
+
+class encoder(tf.keras.layers.Layer):
+   def __init__(self, input_vocab_size, num_layers, d_model, num_heads, dff, maximum_position_encoding = 10000, dropout = 0.0):
+
+    super(encoder, self).__init__()
+    self.d_model = d_model
+    self.embedding = tf.keras.layers.Embedding(input_vocab_size, d_model, mask_zero=True)
+    self.conv1 = layers.Conv1D(d_model, 3, padding="same", activation="relu")
+    self.conv2 = layers.Conv1D(d_model, 3, padding="same", activation="relu")
+    self.conv3 = layers.Conv1D(d_model, 3, padding="same", activation="relu")
+
+    self.dense = layers.Dense(d_model)
+    self.concat = layers.Concatenate(axis=-1)
+    self.repeat = layers.RepeatVector(100)
+    self.pos = positional_encoding(maximum_position_encoding, d_model)
+    self.encoder_layers = [EncoderLayer(d_model = d_model, num_heads = num_heads, dff = dff, dropout = dropout) for _ in range(num_layers)]
+    self.dropout = tf.keras.layers.Dropout(dropout)
+
+   def call(self, F0, mask=None, training=None):
+    x = self.embedding(F0)
+
+    # positional encoding
+    x *= (tf.math.sqrt(tf.cast(self.d_model, tf.float32)))
+    x += (self.pos[: , :tf.shape(x)[1], :])
+    x = self.dropout(x, training=training)
+
+    #Encoder layer
+    embedding_mask = self.embedding.compute_mask(F0)
+    for encoder_layer in self.encoder_layers:
+      x = encoder_layer(x, mask = embedding_mask)
+    return x
+
+   def compute_mask(self, inputs, mask=None):
+    return self.embedding.compute_mask(inputs)

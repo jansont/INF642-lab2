7d6af412f9060b973cd193aaf70fbf8193d6a74c
Theo Janson <tj@sfjtechnologies.com>
2023-01-20 11:48:51 +0100

    .

--- src/EncoderLayer.py
+++ src/EncoderLayer.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from src.MultiHeadedAttention import *
+from src.MultiHeadAttention import *
 
 class EncoderLayer(tf.keras.layers.Layer):
   def __init__(self,  d_model, num_heads, dff, dropout):

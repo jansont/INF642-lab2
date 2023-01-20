0964989408bac651cd9c51ed5f2dbfed6ec49051
Theo Janson <tj@sfjtechnologies.com>
2023-01-20 11:50:02 +0100

    .

--- src/DecoderLayer.py
+++ src/DecoderLayer.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from src.MultiHeadedAttention import *
+from src.MultiHeadAttention import *
 
 class DecoderLayer(tf.keras.layers.Layer):
   def __init__(self,  d_model, num_heads, dff, dropout = 0.0):
